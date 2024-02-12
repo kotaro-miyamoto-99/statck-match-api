dde2ea2d035d895b2bfaaa3770f415befef83419
kotaro-777 <kou.aaa36@icloud.com>
2024-02-12 16:37:42 +0900

    feat

--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
+# CORSを許可するオリジンのリスト
+origins = [
+    "http://localhost:3000",  # Next.jsアプリが動作しているオリジン
+    "https://stack-match.vercel.app",  # 本番環境のオリジン
+]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # 全てのオリジンを許可する場合は ["*"] を使用
+    allow_credentials=True,
+    allow_methods=[
+        "*"
+    ],  # 特定のHTTPメソッドのみを許可する場合は ["GET", "POST"] などを指定
+    allow_headers=["*"],  # 特定のヘッダのみを許可する場合は指定
+)
 
 
 # Dependency
